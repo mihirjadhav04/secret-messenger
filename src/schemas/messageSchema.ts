f7ef411903b8f5cc770282184d63af8f56cd6a59
Mihir Jadhav <jadhavmihir143@gmail.com>
2024-06-07 11:36:46 +0530

    FIRST COMMIT : create a new project and done modeling and written schemas for same using zod.

---
+++ src/schemas/messageSchema.ts
@@ -0,0 +1,9 @@
+import {z} from "zod";
+
+export const MessageSchema = z.object({
+    content: z
+    .string()
+    .min(10, { message: "Content must be of atleast 10 characters."})
+    .max(300,  { message: "Content must be of max 300 characters."})
+
+})
